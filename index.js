0fc4ac0b93ce95517c6bef5a1ea0633d2f665a84
Amro dandashli <amrdandashli@gmail.com>
2021-04-14 18:08:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ function unleashDog(dogName, dogBreed) {
 }
 var routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
 function exerciseDog(dog, breed) {
-  return routine.map(fn => fn(dog, breed))
-}
+//   return routine.map(fn => fn(dog, breed))
+// }
